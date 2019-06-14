da32e4f551dfab97cd312421163799a1cca664a1
ByamBa <43212232+ByamB4@users.noreply.github.com>
2019-06-14 17:55:19 +0800

    Create get-rgb-value.py

---
+++ Programming/code/get-rgb-value.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3.6
+
+from PIL import Image
+im = Image.open('starmap.bmp')
+pix_val = list(im.getdata())
+pix_val_flat = [x for sets in pix_val for x in sets]
+
+flag = ''
+for i in pix_val_flat:
+	if int(i) > 0:
+		flag += chr(int(i))
+
+print(flag)
