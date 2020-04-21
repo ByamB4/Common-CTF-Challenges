42dbdb60dd71865eda1079b37627d56c464479b1
Byambadalai Sumiya <43212232+ByamB4@users.noreply.github.com>
2020-04-21 09:31:14 +0800

    Create angr_template.py

---
+++ Reverse Engineering/Code/angr_template.py
@@ -0,0 +1,10 @@
+import angr
+
+p = angr.Project('./filename', auto_load_libs=False)
+state = p.factory.entry_state()
+sm = p.factory.simulation_manager(state)
+
+sm.explore(find=lambda s: b'CORRECT' in s.posix.dumps(1),
+    avoid=lambda s: b'WRONG' in s.posix.dumps(1))
+
+print(sm.found[0].posix.dumps(0))
