2b956050fd43246aaee7c752e3f2f055504ccc8c
Byambadalai Sumiya <byamba4life@gmail.com>
2020-10-12 21:37:02 +0800

    Update n-is-prime.py

--- Cryptography/src/asymmetric-cipher/rsa/n-is-prime.py
+++ Cryptography/src/asymmetric-cipher/rsa/n-is-prime.py
@@ -1,9 +1,18 @@
+from Crypto.Util.number import inverse, long_to_bytes as ltb
+from sympy import isprime
 from pwn import log
-from sympy isprime
 
-N = REDACTED
+n = 0xDEADBEEF
+e = 0xDEADBEEF
+c = 0XDEADBEEF
 
 log.info('Checking N is prime')
-if not isprime(N):
+if not isprime(n):
   log.warn('N is not prime number')
   exit()
+log.success('N is prime')
+d = inverse(e, n - 1)
+m = pow(c, d, n)
+log.info(f'd: {d}')
+log.info(f'm: {m}')
+log.success(f'flag: {ltb(m).decode()}')
