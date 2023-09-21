b22803b3945b70db3a2dd75b76309177e82ee850
Byambadalai Sumiya <byamba4life@gmail.com>
2023-09-21 20:48:21 +0800

    Update and rename PIL-get-RGB-value.py to PIL_get_rgb_value.py

--- programming/src/PIL_get_rgb_value.py
+++ programming/src/PIL_get_rgb_value.py
@@ -1,13 +1,12 @@
-#!/usr/bin/python3.6
-
 from PIL import Image
-im = Image.open('starmap.bmp')
+
+im = Image.open("starmap.bmp")
 pix_val = list(im.getdata())
 pix_val_flat = [x for sets in pix_val for x in sets]
 
-flag = ''
+flag = ""
 for i in pix_val_flat:
-	if int(i) > 0:
-		flag += chr(int(i))
+    if int(i) > 0:
+        flag += chr(int(i))
 
 print(flag)
