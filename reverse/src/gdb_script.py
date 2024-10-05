6252ebf6f179be4110ee88b549e1f49e3d50a059
Byambadalai Sumiya <byamba4life@gmail.com>
2024-10-05 12:15:35 +0800

    Create gdb_script.py

---
+++ reverse/src/gdb_script.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# use plain gdb
+# gdb -q -x x.py
+import gdb
+from string import *
+
+gdb.execute('set pagination off')
+gdb.execute('file checker')
+gdb.execute('b *main+170')
+
+letters = '_' + digits + ascii_letters + '{}'
+
+flag = ''
+for i in range(49):
+    for j in letters:
+        user_input = flag + j
+        gdb.execute(f"r <<< {user_input}")
+
+        for k in range(len(flag)):
+            gdb.execute(f"x/gx $rdi", to_string=True).split("\t")[-1].replace('\n', '')
+            gdb.execute(f"x/gx $rsi", to_string=True).split("\t")[-1].replace('\n', '')
+            gdb.execute('c')
+
+        a = gdb.execute(f"x/gx $rdi", to_string=True).split("\t")[-1].replace('\n', '')
+        b = gdb.execute(f"x/gx $rsi", to_string=True).split("\t")[-1].replace('\n', '')
+        
+        print('a', a)
+        print('b', b)
+
+        print(f'[{len(flag)}]', j)
+
+        if a == b:
+            flag += j
+            print('[found]', flag)
+            input('continue?')
+            break
