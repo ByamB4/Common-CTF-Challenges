12afd011436efd9445f1052ccd26b7508be75d63
Byambadalai Sumiya <byamba4life@gmail.com>
2025-01-10 17:38:22 +0800

    Update wiener_attack.py

--- crypto/asymmetric-cipher/src/wiener_attack.py
+++ crypto/asymmetric-cipher/src/wiener_attack.py
@@ -1,12 +1,12 @@
 from owiener import attack
 from Crypto.Util.number import long_to_bytes
 
-N = 0xDEADBEEF
+n = 0xDEADBEEF
 e = 0xDEADBEEF
 c = 0xDEADBEEF
-d = attack(e, N)
+d = attack(e, n)
 
 if d:
-    print(long_to_bytes(pow(c, d, N)).decode())
+    print(long_to_bytes(pow(c, d, n)).decode())
 else:
     print('No luck!')
