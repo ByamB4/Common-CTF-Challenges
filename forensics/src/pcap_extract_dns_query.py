333a6a5cff9cf1e3c39575c05ca6a87800c9e562
Byambadalai Sumiya <byamba4life@gmail.com>
2025-02-15 10:37:09 +0800

    Create pcap_extract_dns_query.py

---
+++ forensics/src/pcap_extract_dns_query.py
@@ -0,0 +1,22 @@
+import pyshark
+
+def extract_dns_requests(pcap_file):
+    cap = pyshark.FileCapture(pcap_file, display_filter="dns")
+    
+    dns_requests = set()
+    
+    for packet in cap:
+        try:
+            if hasattr(packet, 'dns') and hasattr(packet.dns, 'qry_name'):
+                dns_requests.add(packet.dns.qry_name)
+        except AttributeError:
+            continue
+    
+    cap.close()
+    return dns_requests
+
+pcap_path = "query.pcap"
+dns_queries = extract_dns_requests(pcap_path)
+
+for query in dns_queries:
+    print(query)
