c55950f38cde083e702297fdb3146839f7a0101d
Byambadalai Sumiya <byamba4life@gmail.com>
2025-02-24 18:59:12 +0800

    Delete web/src/post-nullbyte.py

--- web/src/post-nullbyte.py
+++
@@ -1,9 +0,0 @@
-from requests import post
-
-url = 'https://crypto01.chal.ctf.westerns.tokyo'
-data = b'twctf: please give me the flag of\x00 2020'
-
-
-res = post(url=url, data=data)
-
-print(res.text)
